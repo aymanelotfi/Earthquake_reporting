b98047a5c4383e632ff4e59a2c6d7b98d405a839
Anouar Achghaf <achghafanouar@gmail.com>
2023-09-10 16:47:14 +0200

    add database support

--- app.py
+++ app.py
@@ -1,9 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 
 app = Flask(__name__)
-GoogleMaps(app,key="AIzaSyBJP7ueb4OId5v5Mh4lzNH2k6zX9v6mcsA")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///locations.db"
+db = SQLAlchemy(app)
+GoogleMaps(app, key="AIzaSyBJP7ueb4OId5v5Mh4lzNH2k6zX9v6mcsA")
+
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    date_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    data = db.Column(db.String(255), nullable=False)
+
+
+@app.route("/add_location", methods=["POST"])
+def add_location():
+    data = request.get_json()
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    data_str = data["data"]
+    date = datetime.utcnow
+
+    location = Location(
+        latitude=latitude, longitude=longitude, data=data_str, date_time=date
+    )
+    db.session.add(location)
+    db.session.commit()
+    return jsonify({"message": "Location added successfully"})
+
+
+@app.route("/get_locations", methods=["GET"])
+def get_locations():
+    locations = Location.query.all()
+    location_list = []
+    for location in locations:
+        location_data = {
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "date_time": location.date_time.strftime("%Y-%m-%d %H:%M:%S"),
+            "data": location.data,
+        }
+        location_list.append(location_data)
+    return jsonify({"locations": location_list})
+
 
 @app.route("/")
 def mapview():
@@ -12,28 +56,24 @@ def mapview():
         identifier="view-side",
         lat=37.4419,
         lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
+        markers=[(37.4419, -122.1419)],
     )
+    people_locations = []
+    locations = get_locations["locations"]
+    for location in locations:
+        people_locations.append(
+            {
+                "icon": "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+                "lat": location["latitude"],
+                "lng": location["longitude"],
+                "infobox": location["data"],
+            }
+        )
     sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
+        identifier="sndmap", lat=37.4419, lng=-122.1419, markers=people_locations
     )
-    return render_template('example.html', mymap=mymap, sndmap=sndmap)
+    return render_template("example.html", mymap=mymap, sndmap=sndmap)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
