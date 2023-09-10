58a41de06e91cc4fe1fa0e0faf7c3dc16940ba73
Aymane Lotfi <lotfidevers@gmail.com>
2023-09-10 18:46:25 +0200

    zoom on morocco and add pins

--- app.py
+++ app.py
@@ -4,36 +4,35 @@ from flask_googlemaps import Map
 
 app = Flask(__name__)
 GoogleMaps(app,key="AIzaSyBJP7ueb4OId5v5Mh4lzNH2k6zX9v6mcsA")
-
+markers = []
 @app.route("/")
 def mapview():
     # creating a map in the view
     mymap = Map(
         identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
+        lat=30,
+        lng=-8,
         markers=[(37.4419, -122.1419)]
     )
     sndmap = Map(
         identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
+        lat=30,
+        lng=-8,
         markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
+         Marker(31.2426931303958, -7.985738870163966,"Please Help me  !"),
+         Marker(30.2426931303958, -8.985738870163966,"Please Help me !"),
+         Marker(33.2426931303958, -7.985738870163966,"Please Help me ! Call me at xx"),
+        ],
+        style="height:1200px;width:1200px;margin: auto;",
+        zoom = 5
     )
     return render_template('example.html', mymap=mymap, sndmap=sndmap)
-
+def Marker(latitude,longitude,info):
+    return        {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+             'lat': latitude,
+             'lng': longitude,
+             'infobox': "<b>"+info+"</b>"
+          }
 if __name__ == "__main__":
     app.run(debug=True)
